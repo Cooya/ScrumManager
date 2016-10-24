77cca2daa526da797b41db9386350983d734dfc5
ouaraka <ouaraqua@yahoo.fr>
2016-10-24 21:07:54 +0200

    ajout des pages basiques html de connexion et d'inscription sans bar de menu

---
+++ src/web/signinPage.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+  <link rel="stylesheet" href="assets/css/loginSignin.css" />
+
+    <title>Login</title>
+  </head>
+<body>
+<h1 align ="center">SIGN IN TO SCRUM MANAGER</h1>
+
+<form name="SIGNIN" action="#" method="POST">
+  <table border="0" align="center" cellspacing="2" cellpadding="2">
+    <tr align="center">
+      <td><input type="login" name="login" placeholder="login"></td>
+    </tr>
+    <tr align="center">
+      <td><input type="password" name="password" placeholder="password"></td>
+    </tr>
+    <tr align="center">
+      <td><input type="name" name="name" placeholder="name"></td>
+    </tr>
+    <tr align="center">
+      <td><input type="surname" name="surname" placeholder="surname"></td>
+    </tr>
+     <tr align="center">
+      <td><input type="email" name="email" placeholder="email"></td>
+    </tr>
+    <tr align="center">
+
+      <td colspan="2"><input type="submit" value="Sign in" class="myButton"></td> 
+    </tr>
+
+  </table>
+</form>
+ <footer align="center">By signing up, you agree to the Terms of Service and Privacy Policy, 
+    including Cookie Use. 
+    Others will be able to find you by email or phone number when provided.
+  </footer>
+</body>
+</html>

d2499f65a8a7d8c299b972931c85f958602e128a
frejismail <frejismail>
2016-10-31 16:10:13 +0100

    Modification of code

--- src/web/newProject.php
+++ src/web/newProject.php
@@ -25,15 +25,15 @@ if(!empty($name)){
 			$sql = 'INSERT INTO project VALUES("NULL","'.$name.'","'.$data['id'].'","1","1","","'.$datetime.'","'.$link.'")'; 
 			mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error()); 
 			mysql_close();
-		echo '<p style="color:green;"> Votre ajout a ete bien effectue </p>';
+		echo '<p style="color:green;"> Your project has been added </p>';
 
-	echo '<p style="color:red;"><a href=./newProject.html> Retour </a></p>'; 	
+	echo '<p style="color:red;"><a href=./newProject.html> Back </a></p>'; 	
 				}
 
 
 
 	else {
-		echo '<p style="color:red;"> Veuillez completer la saisie des champs par des valeurs valides! veuillez cliquer <a href=./newProject.html> ici </a> pour recommencer </p>'; 	
+		echo '<p style="color:red;"> Please complete the entry fields with valid values! please click <a href=./newProject.html> here </a> to resume </p>'; 	
 			   }
 
 

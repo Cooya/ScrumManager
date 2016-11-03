12406d543003a140fab238d361f3d5a2012ab473
frejismail <frejismail>
2016-11-03 16:20:58 +0100

    Modification of some links

--- src/web/navBar.php
+++ src/web/navBar.php
@@ -3,7 +3,7 @@
 	if(isset($_SESSION['login'])) {
 		echo '
 			<ul id="horiz_menu">
-				<li class="bouton_menu"><a href="logout.php"><img src="assets/images/logout.png" alt="logout" />Logout</a></li>
+				<li class="bouton_menu"><a href="logOut.php"><img src="assets/images/logout.png" alt="logout" />Logout</a></li>
 				<li class="bouton_menu"><a href="projectListPage.php"><img src="assets/images/list.png" alt="projectlist" />Project List</a></li>
 				<li class="bouton_menu"><a href="newProjectPage.php"><img src="assets/images/newproject.png" alt="newproject" />New Project</a></li>
 				<li class="bouton_menu"><a href="index.php"><img src="assets/images/home.png" alt="home" />Home</a></li>

0b8a07f1820fc2cacf8e176147d0ad4b82a39d7a
frejismail <frejismail>
2016-11-03 19:20:44 +0100

    Restored version of projectlist

--- src/web/projectListPage.php
+++ src/web/projectListPage.php
@@ -21,23 +21,26 @@
 		<?php
 			$sql1 = "SELECT id, name, last_update, creation_date, repository_link FROM project"; 
 			$sql2 = "SELECT login FROM user, project WHERE user.id = project.owner"; 
+			$sql3 = "SELECT login FROM user, project WHERE user.id = project.master"; 
 
 			$result1 = $db->query($sql1);
 			$result2 = $db->query($sql2);
-			$data2 = $result2->fetch();
+			$result3 = $db->query($sql3);
+		
 
 			echo '
 				<table  border="1">
 					<tr>
-						<td><b>Name</b></td><td><b>Owner</b></td><td><b>Last update</b></td><td><b>Creation date</b></td><td><b>Repository Link</b></td>
+						<td><b>Name</b></td><td><b>Owner</b></td><td><b>Master</b></td><td><b>Last update</b></td><td><b>Creation date</b></td><td><b>Repository Link</b></td>
 					</tr>
 			';
 
-			while($data1 = $result1->fetch()) {
+			while($data1 = $result1->fetch() and $data2 = $result2->fetch() and $data3 = $result3->fetch()) {
 			    echo '
 			    	<tr>
 			    		<td><a href="projectIndexPage.php?projectId=' . $data1['id'] . '"><b>' . $data1['name'] . '</b></a></td>
 			    		<td><b>' . $data2['login'] . '</b></td>
+			    		<td><b>' . $data3['login'] . '</b></td>
 			    		<td><b>' . $data1['last_update'] . '</b></td>
 			    		<td><b>' . $data1['creation_date'] . '</b></td>
 			    		<td><b>' . $data1['repository_link'] . '</b></td>

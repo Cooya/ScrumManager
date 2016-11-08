71222da3e4f6306bc7755af0649d1421102e68be
frejismail <frejismail>
2016-11-08 13:51:12 +0100

    adding TASK and US Table and insertion

--- src/db/databaseCreation.sql
+++ src/db/databaseCreation.sql
@@ -37,6 +37,34 @@ CREATE TABLE `contributor` (
 	PRIMARY KEY (`projectId`, `userId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
+
+/* ajout de la table "task" */
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(80) unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(80) NOT NULL,
+  `developper_id` int(80) unsigned DEFAULT NULL,
+  `sprint` int(80) NOT NULL,
+  `duration` int(80) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `developper_id` (`developper_id`),
+  KEY `developper_id_2` (`developper_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+/* ajout de la table "us" */
+
+CREATE TABLE IF NOT EXISTS `us` (
+  `id` int(80) NOT NULL,
+  `description` varchar(80) NOT NULL,
+  `priority` int(80) NOT NULL,
+  `cost` int(80) NOT NULL,
+  `sprint` int(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+---------------------------------------------------------
+
 /* contraintes pour la table "project" */
 ALTER TABLE `project`
 	ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `user` (`id`),
@@ -46,3 +74,11 @@ ALTER TABLE `project`
 ALTER TABLE `contributor`
 	ADD CONSTRAINT `contributor_ibfk_1` FOREIGN KEY (`projectId`) REFERENCES `project` (`id`),
 	ADD CONSTRAINT `contributor_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`id`);
+
+
+/* contraintes pour la table "task" */
+
+ALTER TABLE `task`
+  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`developper_id`) REFERENCES `user` (`id`);
+
+
--- src/db/databaseTest.sql
+++ src/db/databaseTest.sql
@@ -5,3 +5,10 @@ INSERT INTO `user` (`id`, `login`, `password`, `name`, `surname`, `mail`) VALUES
 /* insertion d'un projet */
 INSERT INTO `project` (`id`, `name`, `owner`, `master`, `last_update`, `creation_date`, `repository_link`) VALUES
 (3, 'Test', 1, 1, '2016-10-20 07:14:00', '2016-10-12 00:00:00', 'github.fr/112133');
+
+/* insertion d'une tâche */
+INSERT INTO `task` (`id`, `description`, `developper_id`, `sprint`, `duration`) VALUES (1, 'Tâche 1 : test', '1', '1', '2');
+
+
+/* insertion d'une US */
+INSERT INTO `us` (`id`, `description`, `priority`, `cost`, `sprint`) VALUES ('1', 'US 1 : Test', '1', '2', '1');

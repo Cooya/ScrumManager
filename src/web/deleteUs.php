40051b8a2b8be738fad6eb5f6fb0508878a1586c
Osiris <ismail.ismail-frej@etu.u-bordeaux.fr>
2016-11-16 21:04:41 +0100

    Some code corection

--- src/web/deleteUs.php
+++ src/web/deleteUs.php
@@ -1,4 +1,20 @@
 <?php 
 
-echo 'cooooco';
+	session_start();
+	if(!isset($_SESSION['login'])) {
+		header('Location: login.php');
+	}
+	include 'databaseConnection.php';
+
+	
+
+	$projectId = $_GET['projectId'];
+	$sprint = $_GET['sprint'];
+
+	$sql = "DELETE FROM us WHERE projectId= '.$projectId.' AND sprint= '.$sprint.'";
+	$db->query($sql);
+
+
+
+
 ?>

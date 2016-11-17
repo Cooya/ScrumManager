6234e8bac0808c25679cf66fa9e3dd0c83f0ce48
Osiris <ismail.ismail-frej@etu.u-bordeaux.fr>
2016-11-17 02:02:53 +0100

    delete Us controller

--- src/web/deleteUs.php
+++ src/web/deleteUs.php
@@ -6,15 +6,40 @@
 	}
 	include 'databaseConnection.php';
 
-	
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>ScrumManager</title>
+		<meta charset="utf-8">
+    		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="assets/css/style.css">
+  	</head>
+<body>
+
+
+
+<?php
+
+	include 'navBar.php';
 
 	$projectId = $_GET['projectId'];
 	$sprint = $_GET['sprint'];
+	$specific_Id = $_GET['specific_Id'];
 
-	$sql = "DELETE FROM us WHERE projectId= '.$projectId.' AND sprint= '.$sprint.'";
-	$db->query($sql);
+        $resultcond = $db->query('DELETE FROM us WHERE projectId="'.$projectId.'" AND sprint="'.$sprint.'" AND specific_Id="'.$specific_Id.'"');
+	
+	$resultcond->execute();
 
-
-
+        echo '<p style="color:green;">The us has been successfully deleted click <a href=./backLog.php?projectId='.$projectId.'>here</a> to go back to the backlog.</p>'; 	
 
 ?>
+
+
+
+
+</body>
+</html>

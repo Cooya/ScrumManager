b5f4e970ecba11e0ec70eb755e0845c2b4b60ebc
Nicolas <nicolas.marcy@etu.u-bordeaux.fr>
2016-12-01 21:04:25 +0100

    correction d'un bug à la suppression d'un project (clé étrangère dans la table 'updates')

--- src/web/projectList.php
+++ src/web/projectList.php
@@ -98,9 +98,12 @@
 			else if($_POST['action'] == 'delete') { // suppression d'un projet
 				// suppression des contributeurs du projet (nécessaire du fait de la contrainte de clé étrangère)
 				$sql = "DELETE FROM contributor WHERE projectId = $projectId";
-				$sql2 = "DELETE FROM project WHERE id = $projectId";
-				if(!$db->query($sql) || !$db->query($sql2))
+				$sql2 = "DELETE FROM updates WHERE projectId = $projectId";
+				$sql3 = "DELETE FROM project WHERE id = $projectId";
+				if(!$db->query($sql) || !$db->query($sql2) || !$db->query($sql3)) {
+					var_dump($db->errorInfo());
 					$message = '<p style="color:red">An error has occured when trying to delete this project.</p>';
+				}
 				else{
 					$message = '<p style="color:green">The project has been deleted successfully.</p>';
 					$lastId = $db->lastInsertId();
